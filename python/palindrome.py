663fef99ba3688bfb6fd47b4b69cac469781021a
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-05-27 19:49:21 -0700

    complete

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,12 @@
 def palindrome(word):
-    # Write code here
+    word2=[]
+    word=str(word)
+    for i in word:
+        if i.isalnum()==False:
+            continue
+        else:
+            word2.append(i.lower())
+    if word2== word2[::-1]:
+        return True
+    else:
+        return False
